e0a5436610738534e744b6e356701118a5f406ab
jeffrey chen <jeffreychen2016@gmail.com>
2018-03-21 19:51:33 -0500

    Adding data

---
+++ main.js
@@ -0,0 +1,33 @@
+const travel_diary = [
+    {
+        location:"Japan",
+        path:"imgs/japan.jpg",
+        description:"Japan is a stratovolcanic archipelago consisting of about 6,852 islands. The four largest are Honshu, Hokkaido, Kyushu, and Shikoku, which make up about ninety-seven percent of Japan's land area and often are referred to as home islands."
+    },
+    {
+        location:"China",
+        path:"imgs/china.jpg",
+        description:"Brief Introduction to China. Location The People Republic of China is situated in eastern Asia on the western shore of the Pacific Ocean, with an area of 9.6 million square kilometers. ... Among the rivers totaling 220,000 kilometers in length in China, the Changjiang (Yangtze) and the Huanghe (Yellow) are world known."
+    }, 
+    {
+        location:"America",
+        path:"imgs/america.jpg",
+        description:"The United States of America (USA), commonly known as the United States (U.S.) or America, is a federal republic composed of 50 states, a federal district, five major self-governing territories, and various possessions."
+    },   
+    {
+        location:"Korea",
+        path:"imgs/korea.jpg",
+        description:"South Korea occupies the southern portion of the Korean Peninsula, which extends some 1,100 km (680 mi) from the Asian mainland. This mountainous peninsula is flanked by the Yellow Sea to the west, and the Sea of Japan to the east."
+    }, 
+    {
+        location:"Thailand",
+        path:"imgs/thailand.jpg",
+        description:"Thailand is in the heart of Southeast Asia. Cambodia and Laos border the country to the east and northeast, and Myanmar lies to the northwest. To the west is the Andaman Sea and the Gulf of Thailand, southeast of Burma. The long southern region, connecting with Malaysia, is hilly and forested."
+    }, 
+    {
+        location:"Vietnam",
+        path:"imgs/vietnam.jpg",
+        description:'It was a direct result of the First Indochina War (1946–1954) between France, which claimed Vietnam as a colony, and the communist forces then known as Viet Minh. In 1973 a “third” Vietnam war began—a continuation, actually—between North and South Vietnam but without significant U.S. involvement.'
+    }
+];
+
